bbf8ff84ba5f9e112c3a2d7c0c35c0a4ef72d0e7
attibozs-gif <attibozs@gmail.com>
2026-02-14 20:39:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,24 +15,24 @@ menu3.style.display="none"
 
 let lines;
 fetch("https://raw.githubusercontent.com/attibozs-gif/rpggenerator/refs/heads/main/content.txt")
-  .then(res => res.text())
-  .then(text => {
-    lines = text.split('\n');
-   let parsedLines = [];
-let currentId = null;
-let currentContent = "";
-lines.forEach(line => {
-  if (line.includes(':')) {
-    if (currentId) parsedLines.push({ id: currentId, content: currentContent.trim() });
-    const parts = line.split(':');
-    currentId = parts[0].trim();
-    currentContent = parts.slice(1).join(':').trim();} 
-    else {currentContent += '\n' + line;}});if (currentId) parsedLines.push({ id: currentId, content: currentContent.trim() }); })
-  .catch(err => console.error('hiba:', err));
+.then(res => res.text())
+.then(text => {
+lines = text.split('\n');
+let parsedLines = [];  let currentId = null; let currentContent = "";
+lines.forEach(line => {if (line.includes(':')) {if (currentId) parsedLines.push({ id: currentId, content: currentContent.trim() });
+const parts = line.split(':');
+currentId = parts[0].trim();currentContent = parts.slice(1).join(':').trim();} else {
+currentContent += '\n' + line;}});
+if (currentId) parsedLines.push({ id: currentId, content: currentContent.trim() });
+parsedLines.forEach(obj => {const p = document.getElementById(obj.id);
+if (p) p.innerHTML = obj.content.replace(/\n/g, "<br>");});})
+.catch(err => console.error('hiba:', err));
+
 
 world.addEventListener("click", ()=> {if (menu2.style.display==="none") {menu2.style.display="block"} else {menu2.style.display="none"}});
 gen.addEventListener("click", () => {if (menu3.style.display==="none") {menu3.style.display="block"} else {menu3.style.display="none"}});
 mat.addEventListener("click", () =>{let exist=mat.querySelector("#AlapanyagokCopy");if (exist) {exist.remove()} else {const matcopy=material.cloneNode(true); matcopy.id="AlapanyagokCopy"; mat.appendChild(matcopy); matcopy.style.display="block"}})
+
 
 
 
