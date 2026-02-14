b283f2fba06a90dfd03e36adb86751bc556f117f
attibozs-gif <attibozs@gmail.com>
2026-02-14 19:58:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,16 +13,19 @@ allinfo.forEach(div=> {div.style.display="none"});
 menu2.style.display="none"
 menu3.style.display="none"
 
-let lines
-fetch(https://raw.githubusercontent.com/attibozs-gif/rpggenerator/refs/heads/main/content.txt)
-.then(res =>res.text())
-.then(text => {lines = text.split('\n')
-lines.forEach(line=> {const parts =line.split(':'); const id=parts[0].trim(); const content=parts.slice(1).join(':').trim(); 
-const p=document.getElementById(id); if (p) {p.innerHTML = content}})})
-.catch(err => console.error ('hiba:', err))
+let lines;
+fetch("https://raw.githubusercontent.com/attibozs-gif/rpggenerator/refs/heads/main/content.txt")
+  .then(res => res.text())
+  .then(text => {
+    lines = text.split('\n');
+    lines.forEach(line => {const parts = line.split(':');const id = parts[0].trim();const content = parts.slice(1).join(':').trim();const p = document.getElementById(id);
+      if (p) {p.innerHTML = content.replace(/\n/g, "<br>");}});})
+  .catch(err => console.error('hiba:', err));
+
 
 world.addEventListener("click", ()=> {if (menu2.style.display==="none") {menu2.style.display="block"} else {menu2.style.display="none"}});
 gen.addEventListener("click", () => {if (menu3.style.display==="none") {menu3.style.display="block"} else {menu3.style.display="none"}});
 mat.addEventListener("click", () =>{let exist=mat.querySelector("#AlapanyagokCopy");if (exist) {exist.remove()} else {const matcopy=material.cloneNode(true); matcopy.id="AlapanyagokCopy"; mat.appendChild(matcopy); matcopy.style.display="block"}})
+
 
 
