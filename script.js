aaa36f2380d4f10234854f50273c373051561d15
attibozs-gif <attibozs@gmail.com>
2026-02-21 23:03:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,12 +11,19 @@ const mat =document.querySelector("#mat")
 const g2=document.querySelector ("#Generator")
 const nw=document.querySelector ("#new")
 const cont=document.querySelector ("#content")
+const Triskol=document.querySelector ("#Triskol")
+const Virtus=document.querySelector ("#Virtusok")
+const mat1=document.querySelector ("#Mat1")
+const matcont=document.querySelector("#matcont")
+const aa1=document.querySelector("#aa1")
+const aa2=document.querySelector("#aa2")
 
-
-allinfo.forEach(div=> {div.style.display="none"});
 menu2.style.display="none"
 menu3.style.display="none"
 g2.style.display="none"
+matcont.style.display="none"
+allinfo.forEach(div=> {div.style.display="none"});
+
 
 let lines;
 fetch("https://raw.githubusercontent.com/attibozs-gif/rpggenerator/refs/heads/main/content.txt")
@@ -34,11 +41,23 @@ if (p) p.innerHTML = obj.content.replace(/\n\n/g, "<br>");});})
 .catch(err => console.error('hiba:', err));
 
 world.addEventListener("click", ()=> {if (menu2.style.display==="none") {menu2.style.display="block"} else {menu2.style.display="none"}});
+
 gn.addEventListener("click", () => {if (menu3.style.display==="none") {menu3.style.display="block"} else {menu3.style.display="none"}});
-mat.addEventListener("click", () =>{let exist=cont.querySelector("#AlapanyagokCopy");if (exist) {exist.remove()} else {const matcopy=material.cloneNode(true); matcopy.id="AlapanyagokCopy"; cont.appendChild(matcopy); matcopy.style.display="block"}})
+
+mat.addEventListener("click", () => {let exist=cont.querySelector("#AlapanyagokCopy");if (exist) {exist.remove()} else {const matcopy=material.cloneNode(false); 
+    material.querySelectorAll(":scope > h1, :scope > p").forEach(el=>{matcopy.appendChild(el.cloneNode(true))});
+    const Triskolclone=Triskol.cloneNode (false); 
+    Triskol.querySelectorAll(":scope > h2, :scope > img, :scope > p").forEach(el=>{Triskolclone.appendChild(el.cloneNode(true))});
+    matcopy.appendChild(Triskolclone);  const Virtusclone=Virtus.cloneNode (false); 
+    Virtus.querySelectorAll(":scope > h2, :scope > img, :scope > p").forEach(el=>{Virtusclone.appendChild(el.cloneNode(true))});
+    matcopy.appendChild(Virtusclone);
+    matcopy.id="AlapanyagokCopy"; cont.appendChild(matcopy); matcopy.style.display="block"}})
+
 nw.addEventListener("click", () => {if (g2.style.display==="none") {g2.style.display="block"} else {g2.style.display="none"}});
 
-console.log(mat);      
-console.log(material); 
-console.log(cont);
-console.log("cont:", cont);
+mat.addEventListener("click", () => {if (matcont.style.display==="none") {matcont.style.display="block"} else {matcont.style.display="none"}});
+
+console.log(material.innerHTML)
+console.log(material.querySelectorAll("h2"))
+console.log(material.querySelectorAll("p"))
+console.log(material.querySelectorAll("img"))
