658580f9f31320e9883d77a86c5074cdf452ae26
attibozs-gif <attibozs@gmail.com>
2026-02-24 20:33:28 +0100

    Add files via upload

--- generator.js
+++ generator.js
@@ -3,7 +3,7 @@ const subclass = {}; const kaszt = {}; const charcter = {}; const player = {};
 fetch("https://raw.githubusercontent.com/attibozs-gif/rpggenerator/refs/heads/main/stat.json")
 .then (response => response.json())
 .then (data => {document.addEventListener("DOMContentLoaded", () => {const startSP = document.getElementById("startSP");
-startSP.addEventListener("input", () => {const spValue=Number(startSP.value); const filteredRows = data.Stats.filter (row =>row.SP === spValue); spValue===subclass.startSP;
+startSP.addEventListener("input", () => {const spValue=Number(startSP.value); const filteredRows = data.Stats.find (row =>Number(row.SP) === spValue); spValue=subclass.startSP;
 console.log(filteredRows)}) });})
 
 
