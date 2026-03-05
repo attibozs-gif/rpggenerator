e61537c841438360b708db68068908b9983add20
attibozs-gif <attibozs@gmail.com>
2026-03-05 22:54:55 +0100

    Add files via upload

--- generator.js
+++ generator.js
@@ -1,18 +1,22 @@
 const subclass = {}; const kaszt = {}; const charcter = {}; const player = {}; 
-const Cc = document.querySelector ("#CurrentClass"); const Pc = document.getElementById ("PreviousClass"); const Nc =document.getElementById ("NextClass")
+const Cc = document.querySelector ("#CurrentClass"); const Pc = document.querySelector ("#PreviousClass"); const Nc =document.querySelector ("#NextClass")
+console.log("Pc:", Pc, "Nc:", Nc, "Cc:", Cc);
 
+let cindex=0;
 let dataStore;
 fetch("https://raw.githubusercontent.com/attibozs-gif/rpggenerator/refs/heads/main/stat.json")
-.then (response => response.json())
-.then (data => {dataStore=data}); document.addEventListener("DOMContentLoaded", () => {const startSP = document.getElementById("startSP");
+  .then(response => response.json())
+  .then(data => {dataStore = data; console.log("Classes loaded:", dataStore.Classes); Cc.textContent = dataStore.Classes[cindex]}); 
+document.addEventListener("DOMContentLoaded", () => {const startSP = document.getElementById("startSP");
 startSP.addEventListener("input", () => {let spValue=Number(startSP.value); if (subclass.startSP !== undefined) {spValue=subclass.startSP} 
 const filteredRows = dataStore.Stats.filter (row =>Number(row.SP) === spValue); 
 console.log(filteredRows)}) });
-let cindex=0; Pc.addEventListener("click", () => {if (cindex===0) {cindex=12} else {cindex--}})  
-Nc.addEventListener("click", () => {if (cindex===12) {cindex=0} else {cindex++}; Cc.textContent=dataStore.Classes[cindex];})
-console.log(cindex); 
-
-
+Pc.addEventListener("click", () => {if (!dataStore) { console.log("Prev clicked but dataStore not ready"); return; };
+cindex = cindex === 0 ? dataStore.Classes.length - 1 : cindex - 1; Cc.textContent = dataStore.Classes[cindex];
+    console.log("Prev clicked, cindex =", cindex);});
+Nc.addEventListener("click", () => {if (!dataStore) { console.log("Next clicked but dataStore not ready"); return; }
+cindex = cindex === dataStore.Classes.length - 1 ? 0 : cindex + 1; Cc.textContent = dataStore.Classes[cindex];
+console.log("Next clicked, cindex =", cindex);});
 
 
 
